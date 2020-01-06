9ed1aa250e8274e5bbd3382df8649a1cd1f1457e
Marcos Paes Leme de Souza <marcos.souza@fcamara.com.br>
2020-01-06 11:49:47 -0300

    Implementing calculator rest

---
+++ Aula2_Calculadora/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestwithAspNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+   
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumberic(firstNumber) && IsNumberic(firstNumber)) {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return sum.ToString();
+            }
+            
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalNumber;
+            if(decimal.TryParse(number, out decimalNumber)) {
+                return decimalNumber;
+            }
+            return 0;
+        }
+
+        private bool IsNumberic(string value)
+        {
+            double number;
+            bool isNumber = double.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumber;
+        }
+    }
+}
