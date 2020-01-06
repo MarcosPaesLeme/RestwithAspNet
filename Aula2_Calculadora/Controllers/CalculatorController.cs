c6f002e11466a3a650632c522ed475fef78dc1df
Marcos Paes Leme de Souza <marcos.souza@fcamara.com.br>
2020-01-06 12:01:29 -0300

    Adjust gitignore

--- Aula2_Calculadora/Controllers/CalculatorController.cs
+++ Aula2_Calculadora/Controllers/CalculatorController.cs
@@ -12,14 +12,48 @@ namespace RestwithAspNet.Controllers
     {
    
         // GET api/values/5/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
-        public ActionResult<string> Get(string firstNumber, string secondNumber)
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
         {
             if(IsNumberic(firstNumber) && IsNumberic(firstNumber)) {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return sum.ToString();
+                return Ok(sum.ToString());
             }
-            
+
+            return BadRequest("Invalid input");
+        }
+
+        // GET api/values/5/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Subtraction(string firstNumber, string secondNumber)
+        {
+            if(IsNumberic(firstNumber) && IsNumberic(firstNumber)) {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Division(string firstNumber, string secondNumber)
+        {
+            if(IsNumberic(firstNumber) && IsNumberic(firstNumber)) {
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("multiple/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Multiple(string firstNumber, string secondNumber)
+        {
+            if(IsNumberic(firstNumber) && IsNumberic(firstNumber)) {
+                var multiple = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiple.ToString());
+            }
+
             return BadRequest("Invalid input");
         }
 
